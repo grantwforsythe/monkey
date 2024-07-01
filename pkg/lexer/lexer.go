b6742f6d782941b16f22cb195964f6f78b259dce
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2024-06-30 22:29:55 -0400

    feat: add lexer (#2)
    * feat: parse identifiers and whitespace
    * feat: add support for "==" and "!="
    * fix: broken imports

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -2,10 +2,10 @@ package lexer
 
 import (
 	"github.com/grantwforsythe/monkeylang/pkg/token"
+	"github.com/grantwforsythe/monkeylang/pkg/utils"
 )
 
 // TODO: Fully support on Unicode and UTF-8 characters (See Chapter 1.3 for more info)
-// TODO: Add a linter
 
 type Lexer struct {
 	input        string
@@ -33,12 +33,43 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+// peek the next character without advancing the position of the input string
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readDigit() string {
+	position := l.position
+	for utils.IsDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// It reads an identifier and advances our lexer's postions until it encounters a non-letter character
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for utils.IsLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhiteSpace()
 
 	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -47,16 +78,54 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.RPAREN, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '=':
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: "=="}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '!':
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: "!="}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if utils.IsLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			// Exit early because we do not want to call readChar twice
+			return tok
+		} else if utils.IsDigit(l.ch) {
+			tok.Literal = l.readDigit()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
+
 	l.readChar()
 	return tok
 }
