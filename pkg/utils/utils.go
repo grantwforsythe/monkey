25e66bd739f6f7be88d881a5ed50e24a9bff79d3
Grant Forsythe <grantwforsythe@gmail.com>
2024-07-25 22:41:45 -0400

    Revert "refactor: move errorstring to utils"
    This reverts commit 1670836c49972fe31ba807cca184a4e72da12472.

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,13 +1,5 @@
 package utils
 
-type ErrorString struct {
-	s string
-}
-
-func (e *ErrorString) Error() string {
-	return e.s
-}
-
 func IsLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
