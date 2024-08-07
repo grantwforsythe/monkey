d95e6c1fecb2cbd897a0279bfc4d857d1a163e1d
Grant Forsythe <grantwforsythe@gmail.com>
2024-08-06 20:46:44 -0400

    feat: add monkeyface

--- main.go
+++ main.go
@@ -8,13 +8,26 @@ import (
 	"github.com/grantwforsythe/monkeylang/pkg/repl"
 )
 
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-		user.Username)
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Print(MONKEY_FACE)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
