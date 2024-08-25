aa1fa7eac0b24d40cff458a70f6cb821ec2f9cba
Grant Forsythe <grantwforsythe@gmail.com>
2024-08-25 16:11:13 -0400

    feat: eval functions

--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -124,6 +124,8 @@ func TestReturnStatements(t *testing.T) {
 		{"return 10; 9;", 10},
 		{"return 2 * 5; 9;", 10},
 		{"9; return 2 * 5; 9;", 10},
+		{"fn(x, y){ x + y }(5, 5);", 10},
+		{"fn(x, y){ return x + y }(5, 5);", 10},
 	}
 
 	for _, tt := range tests {
@@ -209,6 +211,64 @@ func TestLetStatement(t *testing.T) {
 		eval := testEval(tt.input)
 		testIntegerObject(t, eval, tt.expected)
 	}
+}
+
+func TestFunctionObject(t *testing.T) {
+	input := "fn(x) { x + 2; };"
+
+	eval := testEval(input)
+
+	fn, ok := eval.(*object.Function)
+	if !ok {
+		t.Fatalf("object is not of type *object.Function. got=%T (%+v)", eval, eval)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong parameters. Parameters=%+v", fn.Parameters)
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("parameter is not 'x'. got=%q", fn.Parameters[0])
+	}
+
+	if fn.Body.String() != "(x + 2)" {
+		t.Fatalf("body is not %q. got=%q", "(x + 2)", fn.Body.String())
+	}
+}
+
+// TODO: Add a recursive function as a test
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestFunctionClosures(t *testing.T) {
+	input := `
+	let x = 100;
+
+	let highFn = fn(x) {
+		let y = 5;
+		return fn(z) { x + y + z };
+	};
+
+	let closure = highFn(2);
+	closure(10);
+	`
+
+	testIntegerObject(t, testEval(input), 17)
 }
 
 func testEval(input string) object.Object {
--- pkg/object/environment.go
+++ pkg/object/environment.go
@@ -2,15 +2,25 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
 	store := make(map[string]Object)
-	return &Environment{store}
+	return &Environment{store: store, outer: nil}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	store := make(map[string]Object)
+	return &Environment{store: store, outer: outer}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	// If the identifier does not exist in the current environment, continously search all other nested environements
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
