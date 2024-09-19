e0db9c3d5f68a8aff46ef7bd1dc3bb06aae4ccab
Grant Forsythe <grantwforsythe@gmail.com>
2024-09-18 22:16:13 -0400

    feat: add unquote macro

--- pkg/ast/modify.go
+++ pkg/ast/modify.go
@@ -4,6 +4,8 @@ type ModifierFunc func(Node) Node
 
 // Apply a modifer to an AST node.
 func Modify(node Node, modifier ModifierFunc) Node {
+	// TODO: Replace underscores with proper error handling
+
 	switch node := node.(type) {
 	case *Program:
 		for i, statement := range node.Statements {
@@ -12,6 +14,65 @@ func Modify(node Node, modifier ModifierFunc) Node {
 
 	case *ExpressionStatement:
 		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+
+	case *CallExpression:
+		for i, argument := range node.Arguments {
+			node.Arguments[i], _ = Modify(argument, modifier).(Expression)
+		}
+
+	case *InfixExpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+
+	case *PrefixExpression:
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+
+	case *IndexEpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Index, _ = Modify(node.Index, modifier).(Expression)
+
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence, _ = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative, _ = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+
+	case *BlockStatement:
+		for i, statement := range node.Statements {
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+
+	case *ReturnStatement:
+		node.ReturnValue, _ = Modify(node.ReturnValue, modifier).(Expression)
+
+	case *LetStatement:
+		node.Value, _ = Modify(node.Value, modifier).(Expression)
+
+	case *FunctionLiteral:
+		if node.Parameters != nil {
+			for i, parameter := range node.Parameters {
+				node.Parameters[i], _ = Modify(parameter, modifier).(*Identifier)
+			}
+		}
+		node.Body, _ = Modify(node.Body, modifier).(*BlockStatement)
+
+	case *ArrayLiteral:
+		for i, element := range node.Elements {
+			node.Elements[i], _ = Modify(element, modifier).(Expression)
+		}
+
+	case *HashLiteral:
+		modifiedPairs := make(map[Expression]Expression)
+
+		for key, value := range node.Pairs {
+			modifiedKey, _ := Modify(key, modifier).(Expression)
+			modifiedValue, _ := Modify(value, modifier).(Expression)
+
+			modifiedPairs[modifiedKey] = modifiedValue
+		}
+
+		node.Pairs = modifiedPairs
 	}
 
 	return modifier(node)
--- pkg/ast/modify_test.go
+++ pkg/ast/modify_test.go
@@ -40,6 +40,72 @@ func TestModify(t *testing.T) {
 				},
 			},
 		},
+		{
+			&InfixExpression{Left: one(), Operator: "+", Right: two()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&InfixExpression{Left: two(), Operator: "+", Right: one()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&PrefixExpression{Operator: "-", Right: one()},
+			&PrefixExpression{Operator: "-", Right: two()},
+		},
+		{
+			&IndexEpression{Left: one(), Index: one()},
+			&IndexEpression{Left: two(), Index: two()},
+		},
+		{
+			&IfExpression{
+				Condition: one(),
+				Consequence: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+				Alternative: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+			},
+			&IfExpression{
+				Condition: two(),
+				Consequence: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+				Alternative: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+			},
+		},
+		{&ReturnStatement{ReturnValue: one()}, &ReturnStatement{ReturnValue: two()}},
+		{&LetStatement{Value: one()}, &LetStatement{Value: two()}},
+		{
+			&FunctionLiteral{
+				Body: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+			},
+			&FunctionLiteral{
+				Body: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+			},
+		},
+		{
+			&ArrayLiteral{Elements: []Expression{one(), two()}},
+			&ArrayLiteral{Elements: []Expression{two(), two()}},
+		},
 	}
 
 	for _, test := range tests {
@@ -47,6 +113,26 @@ func TestModify(t *testing.T) {
 
 		if !reflect.DeepEqual(modified, test.expected) {
 			t.Errorf("not equal. got=%#v, expected=%#v", modified, test.expected)
+		}
+	}
+
+	hashLiteral := &HashLiteral{
+		Pairs: map[Expression]Expression{
+			one(): one(),
+			one(): one(),
+		},
+	}
+
+	Modify(hashLiteral, turnOneIntoTwo)
+
+	for key, val := range hashLiteral.Pairs {
+		key, _ := key.(*IntegerLiteral)
+		if key.Value != 2 {
+			t.Errorf("value is not %d, got=%d", 2, key.Value)
+		}
+		val, _ := val.(*IntegerLiteral)
+		if val.Value != 2 {
+			t.Errorf("value is not %d, got=%d", 2, val.Value)
 		}
 	}
 }
