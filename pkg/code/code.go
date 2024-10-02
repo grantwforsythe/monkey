d87c091e4e4a37df27ea22f24ddecb068d8936e0
Grant Forsythe <grantwforsythe@gmail.com>
2024-10-02 17:48:38 -0400

    feat: add string representation for instruction

--- pkg/code/code.go
+++ pkg/code/code.go
@@ -8,14 +8,56 @@ package code
 // A computer's memory is segmented into "words" - smallest unit of memory - which is usually 32/64 bits.
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"strings"
 )
 
 // Instructions represents instructions for the virtual machine.
 // An instruction is a small, basic command that tells the simulated processor what to do.
 // It is made up of an Opcode and an operator.
 type Instructions []byte
+
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		definition, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		// +1 because the ith position is the opcode
+		operands, offset := ReadOperands(definition, ins[i+1:])
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(definition, operands))
+
+		i += 1 + offset
+	}
+
+	return strings.TrimRight(out.String(), "\n")
+}
+
+// TODO: Rename method
+func (ins Instructions) fmtInstruction(defintion *Definition, operands []int) string {
+	if len(operands) != len(defintion.OperandWidths) {
+		return fmt.Sprintf(
+			"ERROR: operand len %d does not match defined %d\n",
+			len(operands),
+			len(defintion.OperandWidths),
+		)
+	}
+
+	switch len(defintion.OperandWidths) {
+	case 1:
+		// No newline is needed because it is include in ins.String()
+		return fmt.Sprintf("%s %d", defintion.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operand count for %s\n", defintion.Name)
+}
 
 // Opcode represents the "operator" part of an instruction.
 type Opcode byte
@@ -45,20 +87,20 @@ func Lookup(op byte) (*Definition, error) {
 	return def, nil
 }
 
-// Make converts an instruction from a given opcode and associated operands.
-func Make(op Opcode, operands []int) Instructions {
+// Make creates an instruction from a given opcode and associated operands.
+func Make(op Opcode, operands ...int) Instructions {
 	definition, ok := definitions[op]
 	if !ok {
 		// BUG: nil or empty slice?
 		return []byte{}
 	}
 
-	// TODO: Refactor this
-	instruction := make([]byte, definition.OperandWidths[0]+1)
+	// +1 because we need to account for the length of the instruction
+	offset := 1
+	instruction := make([]byte, definition.OperandWidths[0]+offset)
 	instruction[0] = byte(op)
 
 	// Iterate over the defined OperandWidths, take the matching element from the given operands and put it into the instruction.
-	offset := 1
 	for i, operand := range operands {
 		switch definition.OperandWidths[i] {
 		case 2:
@@ -68,4 +110,28 @@ func Make(op Opcode, operands []int) Instructions {
 	}
 
 	return instruction
+}
+
+// ReadOperands is the opposite of Make - converts a definition and instruction to respective opcode and operands.
+// Returns the operands for the instruction and the offset which represents the index of the last operand in the instruction.
+func ReadOperands(definition *Definition, instruction Instructions) ([]int, int) {
+	operands := make([]int, len(definition.OperandWidths))
+	offset := 0
+
+	for i, width := range definition.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(instruction[offset:]))
+		}
+
+		offset += width
+	}
+
+	return operands, offset
+}
+
+// TODO: Figure out what is going on
+// ReadUint16 reads an instruction which is stored in memory using big endian.
+func ReadUint16(instruction Instructions) uint16 {
+	return binary.BigEndian.Uint16(instruction)
 }
