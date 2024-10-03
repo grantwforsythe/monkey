3edc781ccc54fbff2221328f0c89df087a1a7a39
Grant Forsythe <grantwforsythe@gmail.com>
2024-10-02 22:45:10 -0400

    feat: create vm that adds int literals to the sack

---
+++ pkg/vm/vm.go
@@ -0,0 +1,82 @@
+// Package vm contains the stack virutal machine.
+// A stack virutal machine is one in which memory is allocated in The Stake which by convention is where the callstack is implemented.
+// Like physical computers that execute machinecode, virutal machines execute bytecode.
+package vm
+
+import (
+	"fmt"
+
+	"github.com/grantwforsythe/monkeylang/pkg/code"
+	"github.com/grantwforsythe/monkeylang/pkg/compiler"
+	"github.com/grantwforsythe/monkeylang/pkg/object"
+)
+
+// StackSize represents the maximum number of elements in the stack.
+const StackSize = 2048 // This number was abritarily choosen
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	// Instructions
+	stack []object.Object
+	// sp represents a stackpointer which always points to the next free space in the stack.
+	sp int
+}
+
+// New creates a new virtual machine from bytecode.
+func New(bytecode *compiler.ByteCode) *VM {
+	return &VM{
+		constants:    bytecode.Constants,
+		instructions: bytecode.Instructions,
+		stack:        make([]object.Object, StackSize),
+		sp:           0,
+	}
+}
+
+// StackTop gets the top element on the stack.
+// Returns nil if the stack is empty.
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+// Run is the fetch-decode-excute cycle for the virtual machine.
+func (vm *VM) Run() error {
+	// The fetch part.
+	for ip := 0; ip < len(vm.instructions); ip++ {
+
+		// The decode part.
+		op := code.Opcode(vm.instructions[ip])
+
+		// The execute part.
+		switch op {
+		case code.OpConstant:
+			index := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+
+			err := vm.push(vm.constants[index])
+			if err != nil {
+				return err
+			}
+
+		}
+	}
+
+	return nil
+}
+
+// push adds an object to the top of the stack and increments the pointer.
+// Returns an error if a stackover flow occurs.
+func (vm *VM) push(obj object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stackover flow")
+	}
+
+	vm.stack[vm.sp] = obj
+	vm.sp++
+
+	return nil
+}
---
+++ pkg/vm/vm_test.go
@@ -0,0 +1,87 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/grantwforsythe/monkeylang/pkg/ast"
+	"github.com/grantwforsythe/monkeylang/pkg/compiler"
+	"github.com/grantwforsythe/monkeylang/pkg/lexer"
+	"github.com/grantwforsythe/monkeylang/pkg/object"
+	"github.com/grantwforsythe/monkeylang/pkg/parser"
+)
+
+type vmTestCase struct {
+	input    string
+	expected any
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		// BUG: Fix me
+		{"1 + 2", 2},
+	}
+
+	runVmTests(t, tests)
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, test := range tests {
+		program := parse(test.input)
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(comp.ByteCode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.StackTop()
+
+		testExpectedObject(t, test.expected, stackElem)
+	}
+}
+
+func testExpectedObject(
+	t *testing.T,
+	expected any,
+	actual object.Object,
+) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)",
+			actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+	}
+
+	return nil
+}
